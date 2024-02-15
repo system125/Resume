3c49f4e544cd8c70578bc00e99444904adf11952
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-15 18:34:48 +0545

    final changes

--- src/components/SoftSkills.svelte
+++ src/components/SoftSkills.svelte
@@ -3,7 +3,6 @@
         <h3 class="text-2xl text-center">Soft Skills</h3>
         <div class="w-[95%] self-center bg-black h-0.5"></div>
     </section>
-
     <ul>
         <li><p>Team Work</p></li>
         <li><p>Problem Solving</p></li>
