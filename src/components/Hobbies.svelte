24e127517f818fc1c4753d9f02d8b6f368690c9e
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-15 17:18:29 +0545

    Added hobbies styles

--- src/components/Hobbies.svelte
+++ src/components/Hobbies.svelte
@@ -1,8 +1,13 @@
 
-<h3>Hobbies</h3>
+<section class="flex flex-col gap-4 p-2 shadow-sm shadow-black rounded-md">
+    <section class="flex flex-col">
+        <h3 class="text-2xl text-center">Hobbies</h3>
+        <div class="w-[95%] self-center bg-black h-0.5"></div>
+    </section>
 
-<ul>
-    <li>Programming</li>
-    <li>Reading</li>
-    <li>Cooking</li>
-</ul>
+    <ul>
+        <li><p>Programming</p></li>
+        <li><p>Reading</p></li>
+        <li><p>Cooking</p></li>
+    </ul>
+</section>
---
+++ src/components/SoftSkills.svelte
@@ -0,0 +1,13 @@
+<section class="flex flex-col gap-4 p-2 shadow-sm shadow-black rounded-md">
+    <section class="flex flex-col">
+        <h3 class="text-2xl text-center">Soft Skills</h3>
+        <div class="w-[95%] self-center bg-black h-0.5"></div>
+    </section>
+
+    <ul>
+        <li><p>Team Work</p></li>
+        <li><p>Problem Solving</p></li>
+        <li><p>Critical Thinking</p></li>
+        <li><p>Time Management</p></li>
+    </ul>
+</section>
