9302cad569a8f1fccde06930843530994a6e9662
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-15 17:19:03 +0545

    imporved skills layout, centered and added gaps between skill cards

--- src/components/Skills/Skills.svelte
+++ src/components/Skills/Skills.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
+	import SkillLevels from './SkillLevels.svelte';
 	import type { SkillTree } from "$lib";
     export let skillz:SkillTree
 </script>
 
-<section>
-    <h3>{skillz.Category}</h3>
+<section class="flex flex-col p-2 shadow-sm shadow-black rounded-md gap-1">
+    <h3 class="text-2xl text-center">{skillz.Category}</h3>
+    <div class="w-[95%] self-center bg-black h-0.5"></div>
     
     <ul>
     {#each skillz.skills as skill}
        <li>
-        <div class="flex flex-row justify-around">
+        <div class="flex flex-row justify-between px-0 content-center">
             <p>{skill.name}</p>
+            <SkillLevels skillLevel={skill.level} />
         </div>
     </li> 
     {/each}
-    </ul>
+</ul>
 
 </section>
