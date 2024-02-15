33bb591c5b5be70a2a4108e76baa2f21c71416b1
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-15 14:46:26 +0545

    Entered skills data

---
+++ src/lib/skillz.ts
@@ -0,0 +1,56 @@
+
+export interface Skillz{level:number,name:string}
+export interface SkillTree{skills: Skillz[],Category:string}
+
+const programmingSkillz:SkillTree = {
+    Category:"Programming",
+    skills: [
+        {
+            name:"Python",
+            level: 5
+        },
+        {
+            name:"C#",
+            level:4.5
+        },
+        {
+            name:"C++",
+            level:4
+        },
+        {
+            name:"Rust",
+            level: 3
+        }
+    ]
+
+}
+
+const CadSkillz:SkillTree = {
+    Category: "CAD",
+    skills: [
+        {
+            name: "SolidWorks",
+            level: 4.5
+        },
+        {
+            name: "Fusion 360",
+            level: 4.5
+        }
+    ]
+}
+
+const CEASkillz:SkillTree = {
+    Category: "CEA",
+    skills: [
+        {
+            name: "OpenFOAM",
+            level:4.5
+        },
+        {
+            name: "Salome Meca",
+            level: 3
+        }
+    ]
+}
+
+export const Skillz:SkillTree[] = [programmingSkillz,CadSkillz,CEASkillz]
