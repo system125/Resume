5b16d253423320547d208c2d73d3aaaa4dcc82bd
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-15 14:46:07 +0545

    Built Basic Skeleton

---
+++ src/components/Resume.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Header from "./Header.svelte";
+	import Sidebar from "./Sidebar.svelte";
+
+</script>
+
+
+    <!-- Resume content here-->
+    <div id="mainContent" class="flex flex-col w-[100%] h-[100%]">
+        <Header />
+
+        <div class="flex flex-row flex-1">
+            <div id="Work_Education" class="flex-1 flex-col w-[100%]">
+
+            </div>
+
+            <div id="Skillz" class="p-4">
+                <Sidebar />
+            </div>
+    
+        </div>
+    </div>
+
