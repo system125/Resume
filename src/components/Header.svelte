66d60d9df99f9b7b865b3e59c1503db9debb7c25
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-15 14:45:38 +0545

    Created Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+
+<div class="flex flex-row">
+    <!-- Resume image-->
+    
+    <header class="flex flex-row p-4 content-center w-[100%] bg-slate-800">
+        <div class="pr-4">
+            <img src="userIcon.png" class="w-16 aspect-auto"/>
+
+        </div>
+        <div class="flex flex-col flex-1">
+            <h1 class="text-white text-[4rem]">
+                Sanjeev Sapkota
+            </h1>
+
+            <section class="flex flex-row w-[100%] content-center justify-between">
+                <p class="text-white text-xl">
+                    <i class="fa-solid fa-envelope"/>
+                    sanjeevsapkota35@gmail.com
+                </p>
+                <p class="text-white text-xl">
+                    <i class="fa-solid fa-phone"/>
+                    +977 970 601 7727
+                </p>
+            </section>
+            <section>
+                <p class="text-white text-xl">
+                    <i class="fa-solid fa-location-dot"/>
+                    Boharatar, Balaju-16 Kathmandu, Nepal
+                </p>
+            </section>
+        </div>
+
+    </header>
+    
+    <!-- Contact info-->
+    <section class="flex flex-row flex-wrap">
+        <p></p>
+    
+    </section>
+</div>
+
+
+<style>
+
+</style>
