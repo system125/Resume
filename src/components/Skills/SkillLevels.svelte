0db297cf9ff82c3e762878cefed5ba87680b8aea
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-16 14:15:28 +0545

    made skill level smaller

--- src/components/Skills/SkillLevels.svelte
+++ src/components/Skills/SkillLevels.svelte
@@ -4,7 +4,7 @@
 </script>
 
   
-  <div class="ml-2 w-[100px] h-2 bg-slate-400 relative self-center overflow-hidden">
+  <div class="ml-2 w-[50px] h-1 bg-slate-400 relative self-center overflow-hidden">
     <div class="h-[100%] bg-slate-900 absolute top-0 left-0" style={`width: ${skillLevel/5 * 100}%`}></div>
 
     <!-- <div class="filled-level" style={`width: ${skillLevel/5 * 100}%`}></div> -->
