f63b48642c490760c572a79d77af513cf14be650
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-02-16 14:13:32 +0545

    added cgpa, and made  start and end date inline

--- src/components/Education.svelte
+++ src/components/Education.svelte
@@ -9,12 +9,12 @@
         <h3 class="text-slate-700 text-xl">
             {college.degree} - {college.field}
         </h3> 
-        <span class="text-xs w-[4rem]">{college.startDate}-{college.endDate}</span>
+        <span class="text-xs w-[10rem] text-end">{college.startDate}-{college.endDate}</span>
     </div>
     <div class="ml-4 px-1 border-l-2">
         <p class="flex flex-row justify-between span w-[100%] content-center">
             <span>{college.collegeName}</span> 
-            <span class="text-xs">Overall CGPA</span>
+            <span class="text-xs">Overall CGPA: {college.cGpa}</span>
         </p>
     </div>
 </section>
